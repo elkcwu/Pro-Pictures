cdbda395d7e9de7c91ada52f2d8da265c7b4d0fc
Kevin Wu <elkcwu@gmail.com>
2020-05-14 00:38:56 -0700

    these two files are not in use anymore

--- src/main/java/com/kwu/propictures/repository/PicturesRepositoryCustom.java
+++
@@ -1,7 +0,0 @@
-package com.kwu.propictures.repository;
-
-import com.kwu.propictures.model.Picture;
-
-public interface PicturesRepositoryCustom {
-    Picture getPictureByPicIdandUserId(Long picId, Long UserId);
-}
